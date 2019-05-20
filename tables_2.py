c9d3b836fbcbc24695a46a634bd0c5475352e97f
Ksenia Romanova <zeviya25@gmail.com>
2019-05-20 23:59:12 +0300

    Update tables_2.py

--- tables_2.py
+++ tables_2.py
@@ -12,19 +12,16 @@ f6_name = 'sgjp_pages_1205921-1500000.tsv'
 f7_name = 'sgjp_pages_125586-1500000.tsv'
 f8_name = 'sgjp_pages_1390496-1500000.tsv'
 
-df_1 = pd.read_csv(f1_name, sep='\t', usecols=['id', 'wordform'])
-df_2 = pd.read_csv(f2_name, sep='\t', usecols=['id', 'wordform'])
-df_3 = pd.read_csv(f3_name, sep='\t', usecols=['id', 'wordform'])
-df_4 = pd.read_csv(f4_name, sep='\t', usecols=['id', 'wordform'])
-df_5 = pd.read_csv(f5_name, sep='\t', usecols=['id', 'wordform'])
-df_6 = pd.read_csv(f6_name, sep='\t', usecols=['id', 'wordform'])
-df_7 = pd.read_csv(f7_name, sep='\t', usecols=['id', 'wordform'])
-df_8 = pd.read_csv(f8_name, sep='\t', usecols=['id', 'wordform'])
+df_1 = pd.read_csv(f1_name, sep='\t', usecols=['id', 'word_form'])
+df_2 = pd.read_csv(f2_name, sep='\t', usecols=['id', 'word_form'])
+df_3 = pd.read_csv(f3_name, sep='\t', usecols=['id', 'word_form'])
+df_4 = pd.read_csv(f4_name, sep='\t', usecols=['id', 'word_form'])
+df_5 = pd.read_csv(f5_name, sep='\t', usecols=['id', 'word_form'])
+df_6 = pd.read_csv(f6_name, sep='\t', usecols=['id', 'word_form'])
+df_7 = pd.read_csv(f7_name, sep='\t', usecols=['id', 'word_form'])
+df_8 = pd.read_csv(f8_name, sep='\t', usecols=['id', 'word_form'])
 
-
-
-
-C = pd.concat([A,B], ignore_index=True)
+df_m = pd.concat([df_1, df_2, df_3, df_4, df_5, df_6, df_7, df_8], ignore_index=True)
 
 
 
