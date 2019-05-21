c7748e79c705081fdc7c440f57f655f952d43e34
Ksenia Romanova <zeviya25@gmail.com>
2019-05-21 23:50:07 +0300

    Update tables_2.py

--- tables_2.py
+++ tables_2.py
@@ -1,7 +1,16 @@
-# вот тут должна быть прога
 import pandas as pd
 import time
 start_time = time.time()
+
+# принтить пустые
+def f_1(list):
+    i = 0
+
+    for elem in list:
+        if elem != (i+0):
+            if elem != (i+1):
+                print(elem)
+
 
 f1_name = 'sgjp_pages_1-1500000.tsv'
 f2_name = 'sgjp_pages_368311-1500000.tsv'
@@ -22,6 +31,19 @@ df_7 = pd.read_csv(f7_name, sep='\t', usecols=['id', 'word_form'], low_memory=Fa
 df_8 = pd.read_csv(f8_name, sep='\t', usecols=['id', 'word_form'], low_memory=False)
 
 df_m = pd.concat([df_1, df_2, df_3, df_4, df_5, df_6, df_7, df_8], ignore_index=True)
+
+f_name_wf = 'wordforms_polish.tsv'
+
+df_m = df_m.rename(columns={'word_form': 'wordform'})
+
+df_mm = df_m.loc[df_m['wordform'] != 'NA']
+#df_m.dropna(axis=0, how='any', thresh=None, subset=None, inplace=False)
+
+df_m.to_csv(f_name_wf, columns=['wordform'], sep='\t', encoding='utf-8', index=False)
+
+
+
+
 
 
 
